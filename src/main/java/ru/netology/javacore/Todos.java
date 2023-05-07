e4532676d92bb3e7efe46b4ef952b38ce52c66c8
Ant722 <Ant722@mail.ru>
2023-05-07 14:37:15 +0300

    Сервер готов

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,58 @@
 package ru.netology.javacore;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class Todos {
-    //...
+    protected List<Operation> inputOperations;
+    private List<String> tasks;
+    private int maxQuantityTasks;
+
+    public Todos() {
+        this.tasks = new ArrayList<>();
+        this.inputOperations = new ArrayList<>();
+    }
+
+    public void addInputOperations(Operation operation) {
+
+        if (operation.getType().equals("ADD")) {
+            inputOperations.add(operation);
+            this.addTask(operation.getTask());
+        } else if (operation.getType().equals("REMOVE")) {
+            inputOperations.add(operation);
+            this.removeTask(operation.getTask());
+            System.out.println(operation.getTask());
+        } else if (operation.getType().equals("RESTORE")) {
+            this.restoreTask();
+        }
+    }
 
     public void addTask(String task) {
-        //...
+        if (tasks.size() < maxQuantityTasks) {
+            tasks.add(task);
+        }
     }
 
     public void removeTask(String task) {
-        //...
+        tasks.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        tasks.sort(Comparator.naturalOrder());
+        return tasks.toString();
     }
 
+    public void restoreTask() {
+        Operation restoreOperation = inputOperations.get(inputOperations.size() - 1);
+        if (restoreOperation.getType().equals("ADD")) {
+            removeTask(restoreOperation.getTask());
+        } else if (restoreOperation.getType().equals("REMOVE")) {
+            addTask(restoreOperation.getTask());
+        }
+        inputOperations.remove(inputOperations.size() - 1);
+    }
+
+    public void setMaxQuantityTasks(int maxQuantityTasks) {
+        this.maxQuantityTasks = maxQuantityTasks;
+    }
 }
